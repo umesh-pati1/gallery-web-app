c3aaf85e2d1ea7253dc481874da00a884b952d94
Umesh Patil <umesh.patil1@ust.com>
2024-10-10 02:19:01 +0530

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/oOUk3MVaEcePNmhcoKutiIbaH2JcrqO3zDBYFjN1kvTusGnM",
   "https://utfs.io/f/oOUk3MVaEcePZcLnBHtvC37Bjk8gDT4fPriR9IMlJwyuYzVH",
@@ -10,21 +12,22 @@ const mockImages = mockUrls.map((url, index) => ({
   url: url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
-          (image) => (
-            <div key={image.id} className="w-48">
-              <img
-                src={image.url}
-                className="aspect-square object-cover"
-                alt="image"
-              />
-            </div>
-          ),
-        )}
+        {mockImages.map((image) => (
+          <div key={image.id + image.url} className="w-48">
+            <img
+              src={image.url}
+              className="aspect-square object-cover"
+              alt="image"
+            />
+          </div>
+        ))}
       </div>
     </main>
   );
